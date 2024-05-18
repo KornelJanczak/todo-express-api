5d4be7fe7db85a7217757d3f33d5cb75ecd1f359
KornelJanczak <korneljanczak10@gmail.com>
2024-05-18 19:29:01 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
