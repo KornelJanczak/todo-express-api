40433b6f0b5d74b20daca0bea790aefa642136df
KornelJanczak <korneljanczak10@gmail.com>
2024-05-20 19:25:20 +0200

    feat: add migration script

---
+++ migrations/1716224718004_init-todo-table.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("todos", {
+    id: "id",
+    content: { type: "text", notNull: true },
+    priority: { type: "text", notNull: true },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable("todos");
+};
