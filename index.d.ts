ac2e76874020c1d76c64149782a5e468db9053d4
KornelJanczak <korneljanczak10@gmail.com>
2024-05-21 22:43:51 +0200

    feat: add delete todo controller

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+// import { Todo } from "./src/types/todo";
+
+// declare global {
+//   namespace Express {
+//     export interface Request {
+//       // <-- you have to export the interface
+//       todo?: Todo;
+//     }
+//   }
+// }
