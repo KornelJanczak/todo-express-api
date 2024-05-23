fbf2d47202d816452d7c131cf7925a2d3143fd0a
KornelJanczak <korneljanczak10@gmail.com>
2024-05-23 22:16:34 +0200

    feat: edit route params

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,14 +1,20 @@
-import { createTodo, updateTodo } from "../controllers/todo";
+import { createTodo, updateTodo, deleteTodo } from "../controllers/todo";
 import { Router } from "express";
 import { checkSchema } from "express-validator";
-import { todoSchema } from "../schemas/todo";
+import { createTodoSchema, updateTodoSchema } from "../schemas/todo";
 import { validationTodo } from "../middlewares/validationMiddleware";
 
 export default (router: Router) => {
-  router.post("/api/todo", checkSchema(todoSchema), validationTodo, createTodo);
+  router.post(
+    "/api/todo",
+    checkSchema(createTodoSchema),
+    validationTodo,
+    createTodo
+  );
+  router.delete("/api/todo/:id", deleteTodo);
   router.patch(
     "/api/todo/:id",
-    checkSchema(todoSchema),
+    checkSchema(updateTodoSchema),
     validationTodo,
     updateTodo
   );
