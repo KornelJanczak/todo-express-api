1df2993e71b8fc6942b21bdf269d1a75f3546104
KornelJanczak <korneljanczak10@gmail.com>
2024-06-09 17:44:34 +0200

    feat: db query fc

---
+++ src/db/dbQuery.ts
@@ -0,0 +1,52 @@
+import { db } from ".";
+import { Priority } from "./../types/todo";
+import { type ApiRoutes } from "./../types";
+
+interface TodoQuery {
+  id?: string;
+  content?: string | null;
+  priority?: Priority | null;
+  queryText: string;
+  queryType: ApiRoutes;
+}
+
+export const dbQuery = async ({
+  id,
+  content,
+  priority,
+  queryText,
+  queryType,
+}: TodoQuery) => {
+  console.log(id, content, priority, queryText);
+
+  try {
+    let values: any[] | null = [content, priority];
+    let result;
+
+    switch (queryType) {
+      case "create":
+        values = [content, priority];
+        break;
+      case "update":
+        values = [content, priority, id];
+        break;
+      case "delete":
+        values = [id];
+        break;
+      case "get":
+        values = [id];
+        return await db.query(queryText, values);
+      case "getAll":
+        return await db.query(queryText);
+      default:
+        throw new Error("Invalid query type");
+    }
+
+    result = await db.query(queryText, values);
+
+    return result;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Error updating todo");
+  }
+};
