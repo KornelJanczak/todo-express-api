f2a7f59fdd4486940ef433f0cbde27cf538399d5
KornelJanczak <korneljanczak10@gmail.com>
2024-06-30 17:32:11 +0200

    refactor: delete unnecessary lines of code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import routes from "./routes";
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import "dotenv";
 import passport from "passport";
 import session from "express-session";
-// import "./strategies/github-strategy";
 import "./strategies/google-strategy";
 import cookieParser from "cookie-parser";
 import cookieSession from "cookie-session";
@@ -14,8 +13,6 @@ const SESSION_EXPIRY_DATE = 60 * 60000;
 const app: Application = express();
 
 app.listen(PORT, () => {
-  
-  
   console.log("Server is running on port " + PORT);
 });
 
@@ -42,22 +39,3 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use("/", routes());
-
-const GOOGLE_OAUTH_URL = process.env.GOOGLE_OAUTH_URL;
-
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-
-const GOOGLE_CALLBACK_URL = "http%3A//localhost:3000/api/auth/callback/google";
-
-const GOOGLE_OAUTH_SCOPES = [
-  "https%3A//www.googleapis.com/auth/userinfo.email",
-
-  "https%3A//www.googleapis.com/auth/userinfo.profile",
-];
-
-// app.get("/", async (req: Request, res: Response) => {
-//   const state = "some_state";
-//   const scopes = GOOGLE_OAUTH_SCOPES.join(" ");
-//   const GOOGLE_OAUTH_CONSENT_SCREEN_URL = `${GOOGLE_OAUTH_URL}?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_CALLBACK_URL}&access_type=offline&response_type=code&state=${state}&scope=${scopes}`;
-//   res.redirect(GOOGLE_OAUTH_CONSENT_SCREEN_URL);
-// });
