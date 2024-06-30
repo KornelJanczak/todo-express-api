f98e2d8fbaed1faa6134908a804b54385c907e92
KornelJanczak <korneljanczak10@gmail.com>
2024-06-30 17:28:56 +0200

    refactor: delete unnecessary files

--- src/db/auth/authQueriesText.ts
+++
@@ -1,6 +0,0 @@
-export const getCurrentUserText = "SELECT * FROM users WHERE email=$1";
-
-export const createUserText =
-  "INSERT INTO users (id, email) VALUES ($1, $2)";
-
-export const getUserByIdText = "SELECT * FROM users WHERE id=$1";
