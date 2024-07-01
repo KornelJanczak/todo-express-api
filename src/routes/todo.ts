172079cbdfbdb60f63cf33f380e32be6779827ee
KornelJanczak <korneljanczak10@gmail.com>
2024-07-01 18:57:58 +0200

    build: use authmiddleware

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -9,8 +9,10 @@ import { Router } from "express";
 import { checkSchema } from "express-validator";
 import { createTodoSchema, updateTodoSchema } from "../schemas/todo";
 import { validationTodo } from "../middlewares/validationMiddleware";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 export default (router: Router) => {
+  router.use(authMiddleware);
   router.get("/api/todo", getTodos);
   router.get("/api/todo/:id", getTodo);
   router.post(
