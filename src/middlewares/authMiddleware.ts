0143b0c9c071aa86d9ecebbbe34a94137fb188e4
KornelJanczak <korneljanczak10@gmail.com>
2024-07-01 18:57:01 +0200

    feat: add auth middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.isAuthenticated()) return next();
+
+  return res.redirect("/api/auth/google");
+};
