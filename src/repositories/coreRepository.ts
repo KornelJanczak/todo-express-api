d897d833f7a4938330f403a802d45dd9ca5c6c89
KornelJanczak <korneljanczak10@gmail.com>
2024-07-01 18:56:46 +0200

    feat: add more methods

--- src/repositories/coreRepository.ts
+++ src/repositories/coreRepository.ts
@@ -1,4 +1,4 @@
-import { Pool } from "pg";
+import { Pool, QueryResult } from "pg";
 
 export abstract class CoreRepository<T, IdType = string> {
   constructor(
@@ -20,16 +20,47 @@ export abstract class CoreRepository<T, IdType = string> {
     return this.mapToModel(result.rows[0]);
   }
 
-  protected async findOne<K extends keyof T>(
-    field: K,
-    value: T[K]
-  ): Promise<T | null> {
-    const query = `SELECT * FROM ${this.tableName} WHERE ${String(field)} = $1`;
-    const result = await this.pool.query(query, [value]);
-    return this.mapToModel(result.rows[0]) || null;
+  public async update(id: IdType, data: Partial<T>): Promise<T | null> {
+    const fields = Object.keys(data);
+    const values = Object.values(data);
+    const setClause = fields
+      .map((field, index) => `${field} = $${index + 1}`)
+      .join(", ");
+
+    const query = `
+      UPDATE ${this.tableName} 
+      SET ${setClause} 
+      WHERE ${String(this.id)} = $${fields.length + 1} 
+      RETURNING *
+    `;
+
+    const result = await this.pool.query(query, [...values, id]);
+
+    if (result.rows.length === 0) {
+      return null;
+    }
+
+    return this.mapToModel(result.rows[0]);
   }
 
-  protected async findAll() {
+  public async delete(id: IdType): Promise<boolean> {
+    const query = `
+      DELETE FROM ${this.tableName}
+      WHERE ${String(this.id)} = $1
+      RETURNING *
+    `;
+
+    try {
+      const result: QueryResult = await this.pool.query(query, [id]);
+
+      return result.rowCount !== null && result.rowCount > 0;
+    } catch (error) {
+      console.error("Error deleting record:", error);
+      return false;
+    }
+  }
+
+  public async findAll() {
     const query = `SELECT * FROM ${this.tableName}`;
     const result = await this.pool.query(query);
     return this.mapToModel(result.rows[0]);
@@ -37,6 +68,15 @@ export abstract class CoreRepository<T, IdType = string> {
 
   public async findById(id: IdType): Promise<T | null> {
     return this.findOne(this.id, id as T[keyof T]);
+  }
+
+  protected async findOne<K extends keyof T>(
+    field: K,
+    value: T[K]
+  ): Promise<T | null> {
+    const query = `SELECT * FROM ${this.tableName} WHERE ${String(field)} = $1`;
+    const result = await this.pool.query(query, [value]);
+    return this.mapToModel(result.rows[0]) || null;
   }
 
   protected abstract mapToModel(row: any): T;
