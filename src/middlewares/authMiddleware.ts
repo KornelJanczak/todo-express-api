a9707bf788d0cce9bd4d64c33cede73158a529e0
KornelJanczak <korneljanczak10@gmail.com>
2024-07-15 11:29:00 +0200

    refactor: delete jwt token approache in middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,53 +1,15 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
-import { User } from "../models/user";
-
-const JWT_SECRET = process.env.JWT_SECRET || "";
 
 export const authMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log(req.user, "user");
+  console.log("-------------------------");
   console.log("Auth middleware executed");
-  console.log(req.isAuthenticated(), "authenticated");
-  const authHeader = req.headers.authorization;
+  console.log("-------------------------");
 
-  console.log("auth header", authHeader);
+  if (req.isAuthenticated()) return next();
 
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
-
-    jwt.verify(token, JWT_SECRET, (err, user) => {
-      if (err) {
-        return res.sendStatus(403);
-      }
-
-      console.log("user jwt", user);
-
-      next();
-    });
-  } else {
-    res.sendStatus(401);
-  }
-
-  // const authHeader = req.headers["authorization"];
-  // const token = authHeader && authHeader.split(" ")[1];
-
-  // if (token == null) {
-  //   console.log("No token provided");
-  //   return res.sendStatus(401);
-  // }
-
-  // jwt.verify(token, JWT_SECRET, (err, user) => {
-  //   if (err) {
-  //     console.log("Token verification failed:", err.message);
-  //     return res.sendStatus(403);
-  //   }
-
-  //   console.log("Token verified successfully");
-  //   req.user = user as User;
-  //   next();
-  // });
+  return res.status(401).send({ error: "Unauthorized!" });
 };
