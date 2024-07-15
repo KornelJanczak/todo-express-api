4a51c4776351e3be5d077a391e3e098bd063a417
KornelJanczak <korneljanczak10@gmail.com>
2024-07-15 11:28:03 +0200

    refactor: delete unnecessary files

--- src/controllers/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Response, Request } from "express";
-import jwt from "jsonwebtoken";
-
-const JWT_SECRET = process.env.JWT_SECRET || "";
-export const createToken = (req: Request, res: Response) => {
-  const token = jwt.sign({ id: req.user?.id }, JWT_SECRET, { expiresIn: "1h" });
-  console.log("Generated token", token);
-  res.json({ token });
-};
