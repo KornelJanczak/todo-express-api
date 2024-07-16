d2a6adc461feb8b912d0dd4c6dc5d7d3e25d15e0
KornelJanczak <korneljanczak10@gmail.com>
2024-07-16 12:45:32 +0200

    feat: create initialLog fc

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,3 +3,9 @@ import { User } from "../models/user";
 export function isUser(user: any): user is User {
   return user && typeof user.id === "string" && typeof user.email === "string";
 }
+
+export function initialLog(content: string) {
+  console.log("------------------------");
+  console.log(content);
+  console.log("------------------------");
+}
