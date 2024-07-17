bdf0b9660e55c4f65900b115f6a3f207a3aba6d4
KornelJanczak <korneljanczak10@gmail.com>
2024-07-17 12:27:37 +0200

    refactor: add important consol log

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -9,6 +9,8 @@ export const authMiddleware = (
   console.log("Auth middleware executed");
   console.log("-------------------------");
 
+  console.log("Authorized = ", req.isAuthenticated());
+
   if (req.isAuthenticated()) return next();
 
   return res.status(401).send({ error: "Unauthorized!" });
