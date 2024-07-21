217750cfbe2c9d1860de04a7aa808a919874f31a
KornelJanczak <korneljanczak10@gmail.com>
2024-07-21 13:09:50 +0200

    test: create test for get all todos controller

--- src/__tests__/todos.test.ts
+++ src/__tests__/todos.test.ts
@@ -1,11 +1,32 @@
 import { describe, it, test, beforeAll, beforeEach } from "@jest/globals";
 import { getTodos } from "../controllers/todo";
 import { mockRequest, mockResponse } from "../__mocks__";
+import uuid4 from "uuid4";
+import { todoRepository } from "../repositories";
+import { Todo } from "../models/todo";
 
 describe("get todos", () => {
-  it("should get all todos", () => {
-    mockResponse.send = jest.fn();
-    getTodos(mockRequest, mockResponse);
-    expect(mockResponse.send()).toHaveBeenCalledWith(200);
+  it("should get all todos and return 200", async () => {
+    const todos = [{ id: uuid4(), title: "Test Todo" }];
+
+    jest.spyOn(todoRepository, "findAll").mockResolvedValue(todos);
+
+    await getTodos(mockRequest, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+    expect(mockResponse.send).toHaveBeenCalledWith({ todos });
+  });
+
+  it("should return status 200 and an empty list when repository has no todos", async () => {
+    const todos: Todo[] = [];
+    
+    jest.spyOn(todoRepository, "findAll").mockResolvedValue(todos);
+
+    await getTodos(mockRequest, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+    expect(mockResponse.send).toHaveBeenCalledWith({ todos });
   });
 });
+
+describe("get todo", () => {});
