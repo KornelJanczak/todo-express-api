8438fe5b2ca0e0d3f8983791d0a103265bd02cb8
KornelJanczak <korneljanczak10@gmail.com>
2024-07-21 13:22:56 +0200

    refactor: add return values to mockResponse

--- src/__mocks__/index.ts
+++ src/__mocks__/index.ts
@@ -2,5 +2,7 @@ import { Request, Response } from "express";
 
 export const mockRequest = {} as Request;
 export const mockResponse = {
-  send: jest.fn(),
+  send: jest.fn().mockReturnThis(),
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn().mockReturnThis(),
 } as unknown as Response;
