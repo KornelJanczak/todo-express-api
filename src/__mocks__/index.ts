e9a40a7153145a5f0d41b480f5985494c505efc1
KornelJanczak <korneljanczak10@gmail.com>
2024-07-21 18:21:35 +0200

    build: add mocks

--- src/__mocks__/index.ts
+++ src/__mocks__/index.ts
@@ -1,8 +1,14 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
+import uuid4 from "uuid4";
 
-export const mockRequest = {} as Request;
+export const mockRequest = {
+  params: { id: uuid4() },
+} as unknown as Request;
+
 export const mockResponse = {
   send: jest.fn().mockReturnThis(),
   status: jest.fn().mockReturnThis(),
   json: jest.fn().mockReturnThis(),
 } as unknown as Response;
+
+export const mockNext = jest.fn() as unknown as NextFunction;
