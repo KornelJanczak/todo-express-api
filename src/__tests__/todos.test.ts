3a2cb5f845318e36cfd27e2d332d5ee8074895a6
KornelJanczak <korneljanczak10@gmail.com>
2024-07-21 13:23:26 +0200

    test: create test for get todo controller

--- src/__tests__/todos.test.ts
+++ src/__tests__/todos.test.ts
@@ -19,7 +19,7 @@ describe("get todos", () => {
 
   it("should return status 200 and an empty list when repository has no todos", async () => {
     const todos: Todo[] = [];
-    
+
     jest.spyOn(todoRepository, "findAll").mockResolvedValue(todos);
 
     await getTodos(mockRequest, mockResponse);
@@ -29,4 +29,26 @@ describe("get todos", () => {
   });
 });
 
-describe("get todo", () => {});
+describe("get todo", () => {
+  // it("should get one todo and return 200", async () => {
+  //   const todo = { id: uuid4(), title: "Test Todo" };
+
+  //   jest.spyOn(todoRepository, "findById").mockResolvedValue(todo);
+
+  //   await getTodos(mockRequest, mockResponse);
+
+  //   expect(mockResponse.status).toHaveBeenCalledWith(200);
+  //   expect(mockResponse.send).toHaveBeenCalledWith({ todo });
+  // });
+
+  it("should return status 404 when todo is not found", async () => {
+    jest.spyOn(todoRepository, "findById").mockResolvedValue(null);
+
+    await getTodos(mockRequest, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(404);
+    expect(mockResponse.send).toHaveBeenCalledWith({
+      error: "Todo not found !",
+    });
+  });
+});
