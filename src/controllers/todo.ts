f56c6086b85ab2ec0ddb8546990a59a776772082
KornelJanczak <korneljanczak10@gmail.com>
2024-07-28 18:10:21 +0200

    refacotr: rebuild funkcion to handle AppError fc

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -12,10 +12,8 @@ export const getTodo = async (
 ) => {
   try {
     initialLog("Get Todo executed");
-    const todo = await todoRepository.FindById(req.params.id);
+    const todo = await todoRepository.findById(req.params.id);
     if (!todo) throw new AppError("Todo not found!", 404);
-    console.log("Sending todo");
-
     return res.status(200).send({ todo });
   } catch (err) {
     next(err);
@@ -27,31 +25,42 @@ export const getTodos = async (
   res: Response,
   next: NextFunction
 ) => {
-  initialLog("Get todos executed");
-  const todos = await todoRepository.FindAll();
-  return res.status(200).send({ todos });
+  try {
+    initialLog("Get todos executed");
+    const todos = await todoRepository.findAll();
+    return res.status(200).send({ todos });
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const createTodo = async (req: Request, res: Response) => {
-  initialLog("Create todo!!!");
+export const createTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  initialLog(`Creating todo for user`);
 
-  if (!req.user) return res.send({ error: "This user doesn't exist!" });
-
+  if (!req.user) throw new AppError("This user doesn't exist!", 401);
   const userId = req.user.id;
-  const { todo } = req;
 
-  if (!todo?.content || !todo.priority || !userId)
-    return res.status(400).send({ error: "Bad todo credentials!" });
+  if (!req.todo || !req.todo.content || !req.todo.priority || !userId)
+    throw new AppError("Bad todo credentials", 400);
+  const { content, priority } = req.todo;
 
-  const newTodo: Todo = {
-    id: `${uuid4()}`,
-    content: todo.content,
-    priority: todo.priority,
-    user_id: userId,
-  };
+  try {
+    const newTodo: Todo = {
+      id: `${uuid4()}`,
+      content,
+      priority,
+      user_id: userId,
+    };
 
-  const result = await todoRepository.Create(newTodo);
-  return res.status(200).send({ result });
+    const result = await todoRepository.create(newTodo);
+    return res.status(200).send({ result });
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const updateTodo = async (req: Request, res: Response) => {
@@ -70,7 +79,7 @@ export const updateTodo = async (req: Request, res: Response) => {
     priority: todo.priority,
   };
 
-  const result = await todoRepository.Update(todo.id, updatedTodo);
+  const result = await todoRepository.update(todo.id, updatedTodo);
   return res.status(200).send({ result });
 };
 
@@ -78,6 +87,6 @@ export const deleteTodo = async (req: Request, res: Response) => {
   initialLog("Delete todo!!!");
   const todoID: string = req.params.id;
   if (!todoID) return res.sendStatus(400);
-  const result = await todoRepository.Delete(todoID);
+  const result = await todoRepository.delete(todoID);
   return res.status(200).send({ deleted: result });
 };
